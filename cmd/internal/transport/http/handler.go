d14bc8f506c9d2d4b0d18593f681fdd4e5c9b237
Jorge Eduardo Ardila <jeam@tuta.io>
2021-03-29 10:54:24 -0500

    handler

--- cmd/internal/transport/http/handler.go
+++ cmd/internal/transport/http/handler.go
@@ -1,10 +1,27 @@
 package http
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Handler - almacena pointer para servicio dado
-type Hnadler struct{}
+type Hnadler struct {
+	Router *mux.Router
+}
 
-func main() {
-	fmt.Println("vim-go")
+// NewHnadler - retorna un pointer a Handler
+func NewHnadler() *Handler {
+	return &Handler{}
+}
+
+// SetupRoutes - sets ups all routes: router.
+func (h *Hnadler) SetupRoutes() {
+	fmt.Println("Setting up Routes")
+	h.Router = mux.NewRouter()
+	h.Router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(w, "Esto vivo!")
+	})
 }
