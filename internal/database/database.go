978f037bdd34f4203f93dccd7e8beb7812b9a4db
Jorge Eduardo Ardila <jeam@tuta.io>
2021-03-29 17:19:00 -0500

    commit db

--- internal/database/database.go
+++ internal/database/database.go
@@ -13,12 +13,13 @@ func NewDatabase() (*gorm.DB, error) {
 	fmt.Println("Setting up new DB connection")
 
 	dbUsername := os.Getenv("DB_USERNAME")
-	dbPassword := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
 	dbHost := os.Getenv("DB_HOST")
 	dbTable := os.Getenv("DB_TABLE")
 	dbPort := os.Getenv("DB_PORT")
 
-	connectString = fmt.Sprintf("host=%s port=%s user=%s, dbname=%s password=%s sslmode=disable", dbHost, dbPort, dbUsername, dbTable, dbPassword)
+	connectString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", dbHost, dbPort, dbUsername, dbTable, dbPassword)
+	fmt.Println(connectString)
 
 	db, err := gorm.Open("postgres", connectString)
 
