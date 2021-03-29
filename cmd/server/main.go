1d4e1291f7ad977aa3d5d873f2efaa6cae24c0d2
Jorge Eduardo Ardila <jeam@tuta.io>
2021-03-29 10:02:22 -0500

    commit 3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+// App - struct que contiene dependencias, pointers a DB conection.
 type App struct{}
 
 func main() {
 	fmt.Println("Rest api go - jea")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error inicializando la App ")
+	}
 }
