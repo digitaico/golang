1c65cc4162dc4c2d50d4b509427771d8b99d4093
Jorge Eduardo Ardila <jeam@tuta.io>
2021-04-05 18:56:37 -0500

    methods

--- main.go
+++ main.go
@@ -8,6 +8,27 @@ func myfunction(firstName string, apellido string) (string, error) {
 	return firstName + " " + apellido, nil
 }
 
+func addOne() func() int {
+	var x int
+
+	return func() int {
+		x++
+		return x + 1
+	}
+}
+
+type Empleado struct {
+	Name string
+}
+
+func (e *Empleado) UpdateName(newName string) {
+	e.Name = newName
+}
+
+func (e *Empleado) PrintName() {
+	fmt.Println(e.Name)
+}
+
 func main() {
 	days := [...]string{"Dom", "Lun", "Mar", "Mier", "Jue", "Vier", "Sab"}
 	weekdays := days[1:6]
@@ -47,9 +68,20 @@ func main() {
 	fmt.Println(celtic)
 
 	fullName, err := myfunction("elliot", "alderson")
+
 	if err != nil {
 		fmt.Println("Handler error case")
 	}
 	fmt.Println(fullName)
 
+	myFunc := addOne()
+
+	fmt.Println(myFunc())
+	fmt.Println(myFunc())
+	fmt.Println(myFunc())
+	fmt.Println(myFunc())
+
+	var empleado Empleado
+	empleado.Name = "Jorge Eduardo Ardfila Muñoz"
+	empleado.PrintName()
 }
