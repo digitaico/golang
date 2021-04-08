cd4f069d5c87971bb39f9914cedb12f102c3274b
Jorge Eduardo Ardila <jeam@tuta.io>
2021-04-08 10:14:49 -0500

    pointers

---
+++ main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestCalculate(t *testing.T) {
+	var tests = []struct {
+		input    int
+		expected int
+	}{
+		{2, 4},
+		{-1, 1},
+		{0, 2},
+		{-5, -3},
+		{99999, 100001},
+	}
+
+	for _, test := range tests {
+		if output := Calculate(test.input); output != test.expected {
+			t.Error("Test fallo: {} inputed, {} esperado, recibido {}", test.input, test.expected, output)
+		}
+	}
+}
