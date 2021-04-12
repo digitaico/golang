8d5441e9052126865f4845de8a1d8d418e39f9b9
Jorge Eduardo Ardila <jeam@tuta.io>
2021-04-12 17:39:01 -0500

    crud app

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bienvenido a la home page")
+	fmt.Println("Endpoint hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
 
 func main() {
-	fmt.Println("hi, it's GO!")
+	handleRequests()
 }
