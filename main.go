e890e384f00e93cd11edf25febd5eba9dbdb789c
Jorge Eduardo Ardila <jeam@tuta.io>
2021-04-12 20:14:21 -0500

    crud api

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -11,11 +14,43 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint hit: homePage")
 }
 
-func handleRequests() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
 }
 
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+type Article struct {
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
 func main() {
+	Articles = []Article{
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hola 2", Desc: "Prueba articulo 2", Content: "Los puestos de comercio en la ruta de la seda estaban"},
+	}
 	handleRequests()
 }
