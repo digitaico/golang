4d65180f8c0b996f5b6be67a052cd068031c003e
Jorge Eduardo Ardila <jeam@tuta.io>
2021-04-22 09:54:40 -0500

    comparing triplets

---
+++ challenge3.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func compareTriplets(ar []int, br []int) []int {
+	var result []int
+	aSum := 0
+	bSum := 0
+
+	for i := 0; i < int(len(ar)); i++ {
+		if ar[i] > br[i] {
+			aSum++
+		}
+		if ar[i] < br[i] {
+			bSum++
+		}
+	}
+
+	result = append(result, aSum, bSum)
+	return result
+}
+
+func main() {
+	ar := []int{17, 28, 30}
+	br := []int{99, 16, 8}
+
+	fmt.Println(compareTriplets(ar, br))
+}
